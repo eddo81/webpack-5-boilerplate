4168fd7989ded549acc2666126dcd86ce293493a
eddo81 <eduardo_jonnerstig@live.com>
2022-04-21 22:15:53 +0200

    minor updates

--- src/templates/core/src/tools/config/webpack/webpack.dev.conf.js
+++ src/templates/core/src/tools/config/webpack/webpack.dev.conf.js
@@ -1,23 +1,22 @@
 const _CONFIG = require('../index.js');
 const { merge } = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
-const WriteFilePlugin = require('write-file-webpack-plugin');
 
 let webpackConfig = merge(baseWebpackConfig, {
 	devtool: 'eval-cheap-module-source-map',
 
 	watch: true,
 
+  watchOptions: {
+    ignored: [`**/${_CONFIG.directories.assets}`, '**/node_modules'],
+  },
+
 	stats: 'minimal',
 
 	output: {
 		chunkFilename: `${_CONFIG.directories.output.js}[name].js`,
 		pathinfo: true
-	},
-
-	plugins: [
-		new WriteFilePlugin()
-	]
+	}
 });
 
 module.exports = webpackConfig;
